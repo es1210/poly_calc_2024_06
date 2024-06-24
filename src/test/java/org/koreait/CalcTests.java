2ce6c1d218eb029acb661fb48cf2b7d884204eaf
es1210 <dmstn27034@gmail.com>
2024-06-24 14:27:54 +0900

    work-2

---
+++ src/test/java/org/koreait/CalcTests.java
@@ -0,0 +1,25 @@
+package org.koreait;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalcTests {
+
+    @Test
+    @DisplayName("1 + 1 == 2")
+    void t1(){
+        assertThat(Calc.run("1 + 1")).isEqualTo(2);
+    }
+    @Test
+    @DisplayName("2 + 1 == 3")
+    void t2(){
+        assertThat(Calc.run("2 + 1")).isEqualTo(3);
+    }
+    @Test
+    @DisplayName("3 + 1 == 4")
+    void t3(){
+        assertThat(Calc.run("3 + 1")).isEqualTo(4);
+    }
+}
