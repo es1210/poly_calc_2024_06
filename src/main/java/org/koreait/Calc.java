975853e623f73d89706537a8837219a2290c7ffa
es1210 <dmstn27034@gmail.com>
2024-06-26 14:52:46 +0900

    10 + (10 + 5) == 25

--- src/main/java/org/koreait/Calc.java
+++ src/main/java/org/koreait/Calc.java
@@ -1,56 +1,38 @@
 package org.koreait;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Calc {
     public static int run(String exp) {
-        //괄호 제거
-        exp = strpOuterBrackets(exp);
-        exp = exp.trim(); //양 옆에 쓸데없는 공백 제거
-
-        //단일항 들어오면 바로 리턴
+        exp = exp.trim(); // 양 옆의 쓸데없는 공백 제거
+        // 괄호 제거
+        exp = stripOuterBrackets(exp);
+        // 단일항이 들어오면 바로 리턴
         if (!exp.contains(" ")) {
             return Integer.parseInt(exp);
         }
-
         boolean needToMulti = exp.contains("*");
         boolean needToPlus = exp.contains("+") || exp.contains(" - ");
-        boolean needToCompound = needToMulti && needToPlus;
         boolean needToSplit = exp.contains("(") || exp.contains(")");
+        boolean needToCompound = needToMulti && needToPlus;
 
         if (needToSplit) {
-            int bracketsCount = 0;
-            int splitPointIndex = -1;
+            int splitPointIndex = findSplitPointIndex(exp);
 
-            for (int i = 0; i < exp.length(); i++) {
-                if (exp.charAt(i) == '(') {
-                    bracketsCount++;
-                }
-                else if (exp.charAt(i) == ')') {
-                    bracketsCount--;
-                }
-                if (bracketsCount == 0) {
-                    splitPointIndex = i;
-                    break;
-                }
-            }
-            String firstExp = exp.substring(0, splitPointIndex + 1);
-            String secondExp = exp.substring(splitPointIndex + 4);
+            String firstExp = exp.substring(0, splitPointIndex);
+            String secondExp = exp.substring(splitPointIndex + 1);
 
-            char operator = exp.charAt(splitPointIndex + 2);
-
+            char operator = exp.charAt(splitPointIndex);
             exp = Calc.run(firstExp) + " " + operator + " " + Calc.run(secondExp);
 
             return Calc.run(exp);
-
         }
-
-        if (needToCompound) {
+        else if (needToCompound) {
             exp = exp.replaceAll("- ", "+ -");
             String[] bits = exp.split(" \\+ ");
-            int rs = 0;
-            for (int i = 0; i < bits.length; i++) {
-                rs += run(bits[i]); // 재귀함수 사용
-            }
-            return rs;
+            String newExp = Arrays.stream(bits).mapToInt(Calc::run).mapToObj(e -> e + "").collect(Collectors.joining(" + "));
+
+            return run(newExp);
         }
         if (needToPlus) {
             exp = exp.replaceAll("- ", "+ -");
@@ -60,7 +42,6 @@ public class Calc {
                 sum += Integer.parseInt(bits[i]);
             }
             return sum;
-
         } else if (needToMulti) {
             String[] bits = exp.split(" \\* ");
             int sum = 1;
@@ -68,20 +49,42 @@ public class Calc {
                 sum *= Integer.parseInt(bits[i]);
             }
             return sum;
-        } else {
-            return Integer.parseInt(exp);
         }
-        //throw new RuntimeException("해석 불가 : 올바른 계산식이 아니야");
+        throw new RuntimeException("해석 불가 : 올바른 계산식이 아니야");
     }
 
-    private static String strpOuterBrackets(String exp) {
+    private static int findSplitPointIndex(String exp) {
+        int index = findSplitPointIndexBy(exp, '+');
+
+        if (index >= 0) return index;
+
+        return findSplitPointIndexBy(exp, '*');
+    }
+
+    private static int findSplitPointIndexBy(String exp, char findChar) {
+        int brackesCount = 0;
+
+        for (int i = 0; i < exp.length(); i++) {
+            char c = exp.charAt(i);
+
+            if (c == '(') {
+                brackesCount++;
+            } else if (c == ')') {
+                brackesCount--;
+            } else if (c == findChar) {
+                if (brackesCount == 0) return i;
+            }
+        }
+        return -1;
+    }
+
+    private static String stripOuterBrackets(String exp) {
         int outerBracketsCount = 0;
 
         while (exp.charAt(outerBracketsCount) == '(' && exp.charAt(exp.length() - 1 - outerBracketsCount) == ')') {
             outerBracketsCount++;
         }
-        if (outerBracketsCount == 0) {
-        }
+        if (outerBracketsCount == 0) return exp;
         return exp.substring(outerBracketsCount, exp.length() - outerBracketsCount);
     }
 }
--- src/test/java/org/koreait/CalcTests.java
+++ src/test/java/org/koreait/CalcTests.java
@@ -103,13 +103,18 @@ public class CalcTests {
         assertThat(Calc.run("((20 + 20)) + 20")).isEqualTo(60);
     }
     @Test
-    @DisplayName("(20 + 20) + 20 + 20 == 80")
+    @DisplayName("(20 - 20) + 20 + 20 == 40")
     void t20(){
-        assertThat(Calc.run("(20 + 20) + 20 + 20")).isEqualTo(80);
+        assertThat(Calc.run("(20 - 20) + 20 + 20")).isEqualTo(40);
     }
     @Test
     @DisplayName("(10 + 20) * 3 == 90")
     void t21(){
         assertThat(Calc.run("(10 + 20) * 3")).isEqualTo(90);
+    }
+    @Test
+    @DisplayName("10 + (10 + 5) == 25")
+    void t22(){
+        assertThat(Calc.run("10 + (10 + 5)")).isEqualTo(25);
     }
 }
